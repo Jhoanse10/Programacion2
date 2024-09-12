b641b189933b94f6557e2883966cedeadc0d99b8
Jhoanse10 <Johanse10@hotmail.com>
2024-09-11 21:33:18 -0500

    Add files via upload

---
+++ trabajo with ant/src/Formularios/Persona.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Formularios;
+
+/**
+ *
+ * @author JOHAN DIAZ
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private int edad;
+
+    public Persona(String nombre, String apellido, String direccion, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.direccion = direccion;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
+}
