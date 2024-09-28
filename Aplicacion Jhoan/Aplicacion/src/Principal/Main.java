f18eb53c7f6e0f8a84ed6ed39e90bd445efceae4
Jhoanse10 <Johanse10@hotmail.com>
2024-09-28 16:36:28 -0500

    Add files via upload

---
+++ Aplicacion Jhoan/Aplicacion/src/Principal/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+import Ventanas.FrmLogin;
+import Ventanas.FrmAdminClientes;
+import Ventanas.FrmAdminCategoria;
+import Ventanas.FrmProductos;
+/**
+ *
+ * @author el_ja
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       FrmLogin app = new FrmLogin();
+        app.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
